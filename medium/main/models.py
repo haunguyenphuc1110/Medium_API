50409fbf5d2c9192604d25a47fe1b67c0e392878
Admin <ngochieu10041997@gmail.com>
2019-11-30 01:48:06 +0700

    Add history to user model

--- medium/main/models.py
+++ medium/main/models.py
@@ -6,6 +6,7 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # add comment
 
@@ -55,6 +56,7 @@ class Users(models.Model):
     password = models.CharField(max_length=50, blank=True, null=True)
     mac_address = models.CharField(max_length=50, blank=True, null=True)
     last_activity = models.DateTimeField(blank=True, null=True)
+    history = ArrayField(models.CharField(max_length=80, blank=True), size=5)
 
     class Meta:
         managed = False
